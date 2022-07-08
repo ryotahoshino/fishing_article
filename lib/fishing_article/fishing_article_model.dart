0b5ee2093902d5cf49cdd29c7a1beda347d2eaea
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2022-07-08 23:24:39 +0900

    モデルの編集

--- lib/fishing_article/fishing_article_model.dart
+++ lib/fishing_article/fishing_article_model.dart
@@ -3,24 +3,20 @@ import 'package:flutter/material.dart';
 import '../domain/fishing.dart';
 
 class FishingArticleModel extends ChangeNotifier{
-  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
-      .collection('articles')
-      .snapshots();
+  final _userCollection = FirebaseFirestore.instance
+      .collection('articles');
 
   List<Fishing>? articles;
 
-  void fetchFishingArticle(){
-    _usersStream.listen((QuerySnapshot snapshot) {
-      final List<Fishing> articles = snapshot.docs.map((DocumentSnapshot document) {
-        Map<String, dynamic> data =
-        document.data()! as Map<String, dynamic>;
-        final String title = data['title'];
-        final String type = data['type'];
-        return Fishing(title, type);
-      }).toList();
-      this.articles = articles;
-      notifyListeners();
-    });
-
+  void fetchFishingArticle() async {
+    final QuerySnapshot snapshot = await _userCollection.get();
+    final List<Fishing> articles = snapshot.docs.map((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+      final String title = data['title'];
+      final String type = data['type'];
+      return Fishing(title, type);
+    }).toList();
+    this.articles = articles;
+    notifyListeners();
   }
 }
